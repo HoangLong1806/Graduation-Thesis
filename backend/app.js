d3983a92cce961ca6fc542dce88a0e8bc6bbff60
Đặng Hoàng Long <danghoanglong180602@gmail.com>
2024-11-04 16:38:48 +0700

    update backupp

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static("uploads"));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// config
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import all routes
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const product = require("./controller/product");
+app.use("/api/v2/user", user);
+app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+// it's for error handling
+app.use(ErrorHandler);
+
+module.exports = app;
