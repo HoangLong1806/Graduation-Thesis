22294bd60e51638ab18e0daaf3fae3560f654e6d
Đặng Hoàng Long <danghoanglong180602@gmail.com>
2024-11-04 15:59:36 +0700

    fix sign-up

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,3 @@
-export const server = "https://graduation-thesis-m2um.onrender.com/api/v2";
+export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "https://graduation-thesis-m2um.onrender.com/";
-// export const server = "http://localhost:8000/api/v2";
-// export const backend_url = "http://localhost:8000/";
+export const backend_url = "http://localhost:8000/";
