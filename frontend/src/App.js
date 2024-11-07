2626251e53bcc7c5fb0a1010aca9b89013107406
Trần Nam <namwe210@gamil.com>
2024-11-07 23:54:14 +0700

    push updat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Store from "./redux/store.js";
 import { loadSeller, loadUser } from "./redux/actions/user";
-import { ShopDashboardPage, ShopCreateProduct,ShopAllProducts } from "./routes/ShopRoutes.js";
+import { ShopDashboardPage, ShopCreateProduct,ShopAllProducts,ShopCreateEvent } from "./routes/ShopRoutes.js";
 import { useSelector } from "react-redux";
 import { ShopHomePage } from "./ShopRoutes.js";
 import SellerProtectedRoute from "./routes/SellerProtectedRoute.js";
@@ -76,6 +76,14 @@ const App = () => {
             element={
               <SellerProtectedRoute>
                 <ShopAllProducts />
+              </SellerProtectedRoute>
+            }
+          />
+          <Route
+            path="dashboard-create-event"
+            element={
+              <SellerProtectedRoute>
+                <ShopCreateEvent />
               </SellerProtectedRoute>
             }
           />
--- frontend/src/routes/ShopRoutes.js
+++ frontend/src/routes/ShopRoutes.js
@@ -1,11 +1,13 @@
 import ShopDashboardPage from '../pages/Shop/ShopDashboardPage'
 import ShopCreateProduct from '../pages/Shop/ShopCreateProduct'
 import ShopAllProducts from '../pages/Shop/ShopAllProducts'
+import ShopCreateEvent from '../pages/Shop/ShopCreateEvent'
 export 
 {
   ShopDashboardPage,
   ShopCreateProduct,
   ShopAllProducts,
+  ShopCreateEvent,
 }
 
 
