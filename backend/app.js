dc3a7906210ff40d5e04b58163cfc8099c5dd910
Trần Nam <namwe210@gamil.com>
2024-11-08 00:45:01 +0700

    1

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,11 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static("uploads"));
-
-
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -38,8 +37,8 @@ app.listen(3000, () => {
 });
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-// config
 
+// config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "config/.env",
@@ -50,10 +49,11 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
+const event = require("./controller/event");
 app.use("/api/v2/user", user);
 app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
-
+app.use("/api/v2/event", event);
 // it's for error handling
 app.use(ErrorHandler);
 
