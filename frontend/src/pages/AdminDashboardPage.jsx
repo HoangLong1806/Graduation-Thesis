7fc8ae21baa1520834206ac0838e53073800278a
longhoang10189 <danghoanglong180602@gmail.com>
2024-11-21 12:12:58 +0700

    update flex adminsidebar

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -6,16 +6,16 @@ const AdminDashboardPage = () => {
   return (
     <div>
       <AdminHeader />
-      <div className='w-full flex'>
-        <div className="flex items-center justify-between w-ful">
+      <div className="w-full flex">
+        <div className="flex items-start justify-between w-full">
           <div className="w-[80px] 800px:w-[330px]">
             <AdminSidebar active={1} />
           </div>
           <AdminDashboardMain />
         </div>
-      </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AdminDashboardPage 
